c2cecef02325bca81ecdf94ad1898ef12c445f25
vaibhavas777 <vaibhav.mitujn777@gmail.com>
2024-03-02 22:29:54 +0530

    abc

---
+++ src/main/java/com/vas/cicddemo/controller/CiCdController.java
@@ -0,0 +1,15 @@
+package com.vas.cicddemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CiCdController {
+
+    @GetMapping("/greetings/{name}")
+    public String greetings(@PathVariable String name){
+      return "Hi "  +name+ " welcome to the CI/CD demo project with jenkins";
+    }
+}
--- src/test/java/com/vas/cicddemo/CiCdDemoApplicationTests.java
+++ src/test/java/com/vas/cicddemo/CiCdDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.vas.cicddemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest
 class CiCdDemoApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testGreetingsEndpoint() throws Exception {
+
+		String name = "Vaibhav Anand Sharma";
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}", name))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hi "  +name+ " welcome to the CI/CD demo project with jenkins"));
 	}
 
 }
